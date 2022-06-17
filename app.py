d027e5681dd41d63c0c213927253ea7e97cbfbe5
rashmi1219 <rashmi.shinning@gmail.com>
2022-06-17 12:43:46 +0530

    output changed

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return "stating ml project"
+    return "CICD pipeline has been established"
 
 if __name__ == "__main__":
     app.run()
